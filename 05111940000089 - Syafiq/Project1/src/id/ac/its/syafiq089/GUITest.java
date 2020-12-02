c460059baea9ab6d70a8867d40abf7458220be31
Syafiqjos <syafiqjos@gmail.com>
2020-12-02 14:25:28 +0700

    Modified Peko Peko - syafiq

--- 05111940000089 - Syafiq/Project1/src/id/ac/its/syafiq089/GUITest.java
+++ 05111940000089 - Syafiq/Project1/src/id/ac/its/syafiq089/GUITest.java
@@ -5,6 +5,7 @@ public class GUITest {
 	public static void main(String[] args) {
 		System.out.print("GUI Testing");
 		System.out.print("GUI Testing Kedua Peko");
+		System.out.print("GUI Testing Peko Peko");
 	}
 
 }
